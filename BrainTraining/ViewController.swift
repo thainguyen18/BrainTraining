aeb20f191e14cdd1e03b15c72916eb901b3776f8
thainguyen18 <42723566+thainguyen18@users.noreply.github.com>
2020-01-17 17:10:30 -0600

    Initial Commit

---
+++ BrainTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrainTraining
+//
+//  Created by Thai Nguyen on 1/17/20.
+//  Copyright © 2020 Thai Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
